ee721596d5c4ffa8f576642a14cb788e870b1438
Karl Ljungkvist <k.ljungkvist@gmail.com>
2023-09-05 09:31:21 +0200

    finalize bootloader able to launch kernel.bin

--- bootloader.asm
+++ bootloader.asm
@@ -8,13 +8,19 @@ bits 16
 
 
     ;; db 'MY OEM  '               ; OEM name
+BYTES_PER_SECTOR_ADDR equ (0x7c00+0x0b)
     ;; dw 512                      ; bytes per sector
+SECTORS_PER_CLUSTER_ADDR equ (0x7c00+0x0d)
     ;; db 1                        ; sectors per cluster
+RESERVED_SECTORS_ADDR equ (0x7c00+0xe)
     ;; dw 1                        ; reserved sectors
+NUM_FATS_ADDR equ (0x7c00+0x10)
     ;; db 2                        ; num FATs
+NUM_ROOTDIR_ENTRIES_ADDR equ (0x7c00+0x11)
     ;; dw 224                      ; num rootdir entries (224 for floppies, 512 for hard drives)
     ;; dw 2880                     ; tot logical sectors
     ;; db 0xf0                     ; media descriptor (f0h for floppies, f8h for hard drives)
+SECTORS_PER_FAT_ADDR equ (0x7c00+0x16)
     ;; dw 9                        ; logical sectors per FAT
 
 SECTORS_PER_TRACK_ADDR equ (0x7c00+0x18)
@@ -31,187 +37,198 @@ NUMBER_OF_HEADS_ADDR equ (0x7c00+0x1a)
     ;; db 'BIRGER     '            ; volume label
     ;; db 'FAT12   '            ; filesystem type
 
+DATA_REGION_START_SECTOR_ADDR equ 0x7e00
+BUFFER_ADDR equ 0x7f00
+KERNEL_LOAD_SEGMENT equ 0x1000
+KERNEL_LOAD_OFFSET equ 0x0
 
 main:
+	;;---------------------------------------------------------------------------------------------
+    ;; Read root directory into buffer memory area
+	;;---------------------------------------------------------------------------------------------
+    ;; start sector: number of reserved sectors + num_fats*sectors_per_fat
+    mov al, [NUM_FATS_ADDR]
+    mov ah, 0
+    mul word [SECTORS_PER_FAT_ADDR] ; discard dx 
+    add ax, [RESERVED_SECTORS_ADDR]
+    push ax
+    
+    ;; number of sectors: 1 + (num_rootdir_entries * 32 - 1) / bytes per sector
+    mov ax, [NUM_ROOTDIR_ENTRIES_ADDR]
+    shl ax, 5                   ; multiply by 32
+    dec ax
+    div word [BYTES_PER_SECTOR_ADDR] ; ax = ax/bytes_per_sector , dx = ax % bytes_per_sector
+    ;; Assume <256
+    inc al
+    mov dl, al                  ; number of sectors to read
 
-    mov ax, 1                   ; starting at sector 2
-    mov dl, 5                   ; read 5 sectors
+    pop ax                      ; restore  start sector
+
     mov bx, 0                   
     mov es, bx
-    mov bx, 0x7e00
+    mov bx, BUFFER_ADDR
     call read_sectors
 
-    mov bx,0x7e00
+    ;; save sector after root dir
+    ;; mov al, dl
+    ;; mov ah, 0
+    mov dh, 0
+    add ax, dx
+    ;; [RESERVED_SECTORS_ADDR]     
+    mov [DATA_REGION_START_SECTOR_ADDR], ax
 
-.print_byte:
-    mov dl, [bx]
-    call print_hex8
-    mov di, line_end_str
+	;;---------------------------------------------------------------------------------------------
+    ;; look for kernel name in root directory entries
+	;;---------------------------------------------------------------------------------------------
+    
+    mov bx, [NUM_ROOTDIR_ENTRIES_ADDR]
+    mov ax, BUFFER_ADDR
+.check_entry:
+    mov di, ax
+    cmp byte [di], 0              ; if first byte is 0
+    jz kernel_file_not_found
+
+    mov cx, 11                  ; compare up to 11 chars
+    mov di, ax
+    mov si, kernel_filename
+    repe cmpsb                   ; compares di, si until either is 0 or they are unequal, then zf is set if they are all equal
+    je kernel_file_found
+
+    add ax, 32
+    dec bx
+    jnz .check_entry
+
+kernel_file_not_found:  
+    mov di, error_kernel_file_not_found_msg
+    call print_str
+    jmp wait_for_keypress_and_reboot
+
+kernel_file_found:  
+
+    ;; save start cluster from root dir
+    mov bx, ax
+    mov word dx, [bx + 26]
+    push dx
+    ;; save size from root dir                            
+    ;; mov dword ebx, [bx+28]
+    ;; push ebx
+
+    mov di, success_msg_part1
+    call print_str
+    mov di, kernel_filename
+    call print_str
+    mov di, success_msg_part2
     call print_str
 
-    inc bx
-    cmp bx, 0x7e10
-    jl .print_byte
 
-    ;; mov dl, [0x7e01]
-    ;; call print_hex8
-    ;; mov di, line_end_str
-    ;; call print_str
+	;;---------------------------------------------------------------------------------------------
+    ;; load FAT
+	;;---------------------------------------------------------------------------------------------
 
-    ;; mov dl, [0x7e02]
-    ;; call print_hex8
-    ;; mov di, line_end_str
-    ;; call print_str
+    ;; start sector: number of reserved sectors
+    mov al, [RESERVED_SECTORS_ADDR]
+    mov ah, 0
+    ;; number of sectors: sectors per fat
+    mov dx, [SECTORS_PER_FAT_ADDR]
+    mov bx, 0                   
+    mov es, bx
+    mov bx, BUFFER_ADDR
+    call read_sectors
+    
+	;;---------------------------------------------------------------------------------------------
+    ;; read kernel file sectors
+	;;---------------------------------------------------------------------------------------------
 
-    ;; Read root directory into memory area
+    ;; In loop:
+    ;; cx is next cluster
+    ;; bx is destination offset
 
-    ;; loop over root directory
+    pop cx                      ; start cluster
+    mov bx, KERNEL_LOAD_OFFSET
+    
+.read_cluster:
+    ;; lba (sector) = data_region_start_sector + (cluster-2)*sectors_per_cluster
+
+    ;; number of sectors to read: sectors per cluster
+    mov dl, [SECTORS_PER_CLUSTER_ADDR]
+    mov dh, 0
+
+    ;; ax is sector addr
+    mov ax, cx
+    sub ax, 2
+    mul dx
+    add ax, [DATA_REGION_START_SECTOR_ADDR]
+
+    ;; number of sectors to read: sectors per cluster
+    mov dl, [SECTORS_PER_CLUSTER_ADDR]
+
+    ;;  set write segment already
+    push es
+    push word KERNEL_LOAD_SEGMENT                   
+    pop es
+    call read_sectors
+    pop es
+
+    ;; ax, dx free to use
+    mov ax, cx
+    mov dx, 3
+    mul word dx
+    shr ax, 1                      ; ax is now (3*cluster)/2
+    add ax, BUFFER_ADDR
+    mov si, ax
+    mov ax, [si]
+    ;; if odd shift down 4 bits, else and with 0xfff
+    test cx, 1                  ; check for even/odd
+    jz .even_cluster
+.odd_cluster:
+    shr ax, 4
+    jmp .fat_entry_read
+.even_cluster:
+    and ax, 0xfff
+.fat_entry_read:
+
+    ;; new cluster
+    mov cx, ax
+    ;; new destination
+    add bx, [SECTORS_PER_CLUSTER_ADDR]
+
+    ;; if fat entry has NOT ff8 set, then read it into cx and jmp .read_cluster
+    and ax, 0xff8
+    cmp ax, 0xff8
+    jne .read_cluster
+
+	;;---------------------------------------------------------------------------------------------
+    ;; Jump into kernel
+	;;---------------------------------------------------------------------------------------------
+    mov di, launch_msg
+    call print_str
 
 
+    mov ax, KERNEL_LOAD_SEGMENT
+    mov ds, ax
+    mov es, ax
+    ;; push ax
+    ;; mov ax, KERNEL_LOAD_OFFSET
+    ;; push ax
+    ;; retf
+    jmp KERNEL_LOAD_SEGMENT:KERNEL_LOAD_OFFSET
 
-
-    ;; mov di, msg
-    ;; call print_str
-
-    ;; HALT here
     cli
     hlt
 .halt:
     jmp .halt
 
 
-;; print_bios_disk_params: 
-;;     mov ah, 8
-;;     mov dl, 0
-;;     mov di, 0
-;;     mov es, di
-;;     int 13h
-;;     ;; bl drive type                  
-;;     ;; ch low 8 bits of max cylinder num
-;;     ;; cl max sector num (5-0), high two bits of max cylinder num (7-6)
-;;     ;; dh maximum head number
-;;     ;; dl numnber of drives
-;;     ;; es:di drive parameter table
-;;     push bx
-;;     push cx
-;;     push dx
-;;     push es
-;;     push di
+wait_for_keypress_and_reboot:   
+    mov di, press_any_key_to_reboot_msg
+    call print_str
 
-;;     ;; drive type: 4 ->2.88M floppy
-;;     ;; heads: 2
-;;     ;; drives: 1
-;;     ;; max cylinder num: 0b00:0x4f  79 -> 80 cylinders/tracks
-;;     ;; max sector num: 0x12 = 18 -> 18 sectors
+    ;; read keystroke    
+    mov ah, 0
+    int 0x16
 
-;;     ;; print di
-;;     pop di
-;;     call print_hex16
-;;     mov di, line_end_str
-;;     call print_str
-
-;;     ;; print es
-;;     pop di
-;;     call print_hex16
-;;     mov di, line_end_str
-;;     call print_str
-
-;;     ;; print dx
-;;     pop di
-;;     call print_hex16
-;;     mov di, line_end_str
-;;     call print_str
-
-;;     ;; print cx
-;;     pop di
-;;     call print_hex16
-;;     mov di, line_end_str
-;;     call print_str
-
-;;     ;; print bx
-;;     pop di
-;;     call print_hex16
-;;     mov di, line_end_str
-;;     call print_str
-
-;;     ret
-
-;; print_hex16:  
-;;     args:
-;;        di - value to print
-;;
-;;     mov bx, 4
-
-;; .print_nibble:
-;;     xor al,al
-;;     mov cx,4
-;; .loop:
-;;     shl di, 1
-;;     rcl al, 1
-;;     dec cx
-;;     jnz .loop
-
-;;     cmp al, 9
-;;     jg .alpha
-;;     add al, '0'
-;;     jmp .hexification_done
-
-;; .alpha:    
-;;     sub al, 10
-;;     add al, 'a'
-;; .hexification_done:
-;;     push bx
-;;     ;; al is byte
-;;     mov ah, 0eh
-;;     mov bh, 00h
-;;     mov bl, 09h
-;;     int 10h
-
-;;     pop bx
-;;     dec bx
-;;     jnz .print_nibble
-
-;;     ret
-
-print_hex8:  
-    ;; args:
-    ;;   dl - number
-    pusha
-    mov bx, 2
-
-.print_nibble:
-    xor al,al
-    mov cx,4
-.loop:
-    shl dl, 1
-    rcl al, 1
-    dec cx
-    jnz .loop
-
-    cmp al, 9
-    jg .alpha
-    add al, '0'
-    jmp .hexification_done
-
-.alpha:    
-    sub al, 10
-    add al, 'a'
-.hexification_done:
-    push bx
-    ;; al is byte
-    mov ah, 0eh
-    mov bh, 00h
-    mov bl, 09h
-    int 10h
-
-    pop bx
-    dec bx
-    jnz .print_nibble
-
-    popa
-
-    ret
-
+    ;; reboot
+    int 0x19
 
 print_char: 
     mov ah, 0eh
@@ -262,12 +279,21 @@ read_sectors:
     popa                        ; restore all registers
     ret
 
-msg:
-    db "banan",0
+press_any_key_to_reboot_msg:
+    db `Press any key to reboot\r\n\0`
 line_end_str:
     db `\r\n\0`
+error_kernel_file_not_found_msg:  
+    db `Error: Could not find kernel file\r\n\0`
+success_msg_part1:  
+    db `Kernel image \"\0`
+success_msg_part2:  
+    db `\" found.\r\n\0`
+
+launch_msg:
+    db `Launching kernel at 0x1000:0x0000\r\n\r\n\0`
 
 kernel_filename:
-    db "kernel.bin"
+    db "KERNEL  BIN",0
 times 510-CODE_START_OFFSET-($-$$) db 0
 ;;     dw 0AA55h
