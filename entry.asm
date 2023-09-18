eb79ca4be124fcb41b5effc1e409cf991ba84def
Karl Ljungkvist <k.ljungkvist@gmail.com>
2023-09-18 08:22:24 +0200

    Add read and printing memory map

--- entry.asm
+++ entry.asm
@@ -6,6 +6,10 @@ global entry
 
 extern kmain
 
+MEM_MAP_BUFFER_SEGMENT   equ 0x7e0
+MEM_MAP_BUFFER_OFFSET    equ 0x0
+MEM_MAP_READ_SIZE equ 24
+
 entry:  
 
     ;; setup stack
@@ -13,6 +17,21 @@ entry:
     mov ss, ax
     mov sp, 0x7000
     mov bp, sp
+
+    ;; get memory map
+
+    mov eax, MEM_MAP_BUFFER_SEGMENT
+    mov es, eax
+    mov di, MEM_MAP_BUFFER_OFFSET
+
+    call read_mem_map
+
+    mov di, MEM_MAP_NUM_STR
+    call print_str
+    mov di, ax
+    call print_hex16
+    call print_newline
+
 
     cli         ; disable interrupts
 
@@ -29,6 +48,82 @@ entry:
     mov cr0, eax
 
     jmp dword 0x08:protected_mode     ; selector 1, in GDT, Privilege level 0
+
+MEM_MAP_NUM_STR:
+    db `Number of mem map entries read: \0`
+
+read_mem_map:   
+    ;; args:
+    ;;   es - dst segment
+    ;;   di - dst offset
+
+    ;; cdecl; eax, ecx, and edx can be trashed
+    push ebx
+
+    xor ebx, ebx                ; start value 0
+
+.mem_map_next:
+    mov ecx, MEM_MAP_READ_SIZE ; size of each_entry
+    mov eax, 0xe820
+    mov edx, 0x534D4150        ; 'SMAP'
+    int 0x15
+
+    ;; ebx == 0, done
+    jc .mem_map_error_carry
+
+    cmp eax, "PAMS" ;0x534D4150         ; 'SMAP'
+    jne .mem_map_error_smap
+
+    cmp ecx, 20
+    jl .mem_map_error_overrun
+    cmp ecx, MEM_MAP_READ_SIZE
+    jg .mem_map_error_underrun
+
+    ;; success
+    cmp ebx, 0
+    je .mem_map_done
+
+    add di, MEM_MAP_READ_SIZE
+    jmp .mem_map_next
+
+.mem_map_error_carry:
+    mov di, .carry_error_str
+    jmp .error
+.mem_map_error_smap:
+    mov di, .smap_error_str
+    jmp .error
+.mem_map_error_overrun:
+    mov di, .overrun_error_str
+    jmp .error
+.mem_map_error_underrun:
+    mov di, .underrun_error_str
+    jmp .error
+
+.error:
+    call print_str
+.infloop:
+    hlt
+    jmp .infloop
+    
+.mem_map_done:
+    mov ax, di
+    sub ax, MEM_MAP_BUFFER_OFFSET
+    mov bl, 24
+    xor dx,dx
+    div bl
+
+    pop ebx
+
+    ret
+
+.carry_error_str:
+    db `Error reading memory map: Carry set\r\n\0`
+.smap_error_str:
+    db `Error reading memory map: Missing SMAP\r\n\0`
+.overrun_error_str:
+    db `Error reading memory map: Overrun\r\n\0`
+.underrun_error_str:
+    db `Error reading memory map: Underrun\r\n\0`
 
 gdt:
 .gdt_null:
@@ -63,7 +158,6 @@ gdtr:
 
 msg_a20_success:  
     db `Successfully set up A20\r\n\0`
-    
 
 protected_mode:
 
--- kernel.c
+++ kernel.c
@@ -1,3 +1,5 @@
+#include "kaos_int.h"
+
 
 char *vga_memory = (char *)0xB8000;
 int cursor_position=0;
@@ -10,22 +12,60 @@ void clear_screen()
     }
 }
 
-void print_char(char c)
+void print_char(char c, char style)
 {
-    vga_memory[cursor_position*2] = c;
-    vga_memory[cursor_position*2+1] = 0x0a;
-    cursor_position++;
+    if(c == '\n') {
+        cursor_position = (cursor_position/80 + 1)*80;
+    } else {
+        vga_memory[cursor_position*2] = c;
+        vga_memory[cursor_position*2+1] = style;
+        cursor_position++;
+    }
 }
 
 void print_str(const char *msg)
 {
     while(*msg != 0)
     {
-        print_char(*msg);
+        print_char(*msg, 0x07);
         msg++;
     }
+}
 
-    cursor_position = (cursor_position/80 + 1)*80;
+void print_hex4(uint8_t v)
+{
+    char c = v<10 ? '0'+v : 'a'+(v-10);
+    print_char(c, 0x07);
+}
+
+void print_hex8(uint8_t v)
+{
+    print_hex4(v>>4);
+    print_hex4(v&0xf);
+}
+
+#define PRINT_NIBBLES(v,n)                      \
+    do {                                        \
+        for(int i=(n-1); i>=0; i--) {           \
+            print_hex4((v>>(i*4))&0xf);         \
+        }                                       \
+    }  while(0)
+
+    
+
+void print_hex16(uint16_t v)
+{
+    PRINT_NIBBLES(v,4);
+}
+
+void print_hex32(uint32_t v)
+{
+    PRINT_NIBBLES(v,8);
+}
+
+void print_hex64(uint64_t v)
+{
+    PRINT_NIBBLES(v,16);
 }
 
 void hang()
@@ -35,11 +75,45 @@ void hang()
     }
 }
 
+
+typedef struct __attribute__((__packed__))
+{
+    uint64_t base;
+    uint64_t size;
+    uint32_t type;
+    uint32_t extra;
+} MemoryMapEntry;
+
+MemoryMapEntry *memory_map = (MemoryMapEntry *)0x7e00;
+
+void print_mem_map()
+{
+    print_str("Base               Size               Type\n");
+    for(int i=0; i<5; i++)
+    {
+        print_hex64(memory_map[i].base);
+        print_str("   ");
+        print_hex64(memory_map[i].size);
+        print_str("   ");
+        if(memory_map[i].type == 1) {
+            print_str("M      ");
+        } else if(memory_map[i].type == 2) {
+            print_str("R      ");
+        } else {
+            print_str("?      ");
+        }
+        print_str("\n");
+    }
+}
+
 void kmain()
 {
 
-    clear_screen();
-    print_str("Hello from kernel.c!");
+    cursor_position = 80*16;
+    print_str("Hello from kernel.c!\n");
+
+    print_str("Memory map:\n");
+    print_mem_map();
 
     hang();
 }
