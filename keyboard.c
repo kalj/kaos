875fc4eb89f0e96e59173a92d01eb31af84182f1
Karl Ljungkvist <k.ljungkvist@gmail.com>
2023-09-29 21:33:17 +0200

    clean up

--- keyboard.c
+++ keyboard.c
@@ -11,13 +11,6 @@ __attribute__((interrupt)) void keyboard_handler(void *irq_frame) {
     if (status & 0x01) {
         uint8_t keycode = portio_inb(0x60);
         strfmt_u8_hex(buf,10,keycode);
-        /* kaos_puts("0x"); */
-        /* uint8_t nibble = (keycode >> 4) & 0xf; */
-        /* char c         = nibble < 10 ? '0' + nibble : 'a' + (nibble - 10); */
-        /* kaos_putc(c); */
-        /* nibble = keycode & 0xf; */
-        /* c      = nibble < 10 ? '0' + nibble : 'a' + (nibble - 10); */
-        /* kaos_putc(c); */
         buf[2] = ' ';
         buf[3] = '\0';
         kaos_puts(buf);
