76ce30377f28aca991cb1c76d608bd6c885f337a
Karl Ljungkvist <k.ljungkvist@gmail.com>
2023-10-11 22:25:14 +0200

    Fixup missing i8254x stuff

---
+++ kernel/i8254x.c
@@ -0,0 +1,5 @@
+#include "i8254x.h"
+
+void i8254x_init(int pci_bus, int pci_dev, int pci_func)
+{
+}
--- kernel/i8254x.h
+++ kernel/i8254x.h
@@ -1,7 +1,6 @@
 #ifndef I8254X_H
 #define I8254X_H
 
-
-void i8254x_init();
+void i8254x_init(int pci_bus, int pci_dev, int pci_func);
 
 #endif /* I8254X_H */
