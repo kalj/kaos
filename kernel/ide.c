90cb34f04593482bc1913f989ae291a5a25e6ad1
Karl Ljungkvist <k.ljungkvist@gmail.com>
2023-10-12 08:39:52 +0200

    dummy IDE driver

---
+++ kernel/ide.c
@@ -0,0 +1,7 @@
+#include "ide.h"
+
+#include "pci.h"
+
+void ide_init(const struct PciEntry *pci)
+{
+}
---
+++ kernel/ide.h
@@ -0,0 +1,8 @@
+#ifndef IDE_H
+#define IDE_H
+
+struct PciEntry;
+
+void ide_init(const struct PciEntry *pci);
+
+#endif /* IDE_H */
